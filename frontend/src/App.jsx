d9ed40158c9a7993de6357a89d845ba4e72891e1
soodshivansh <soodshivansh5@gmail.com>
2024-08-11 00:15:57 +0530

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <div className='flex justify-center items-center h-screen'>
-        <Button>Hello shadcn</Button>
+        <Button>Hello</Button>
       </div>
     </>
   )
